957692b5906f585e1231d98351e563dde63667e1
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-27 13:05:41 +0530

    Getter & Setter methods to access private attributes in main class

--- src/GetterSetter.java
+++ src/GetterSetter.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class GetterSetter {
+//use getter & setter method to access private attributes .
+
+class Employee
+{
+ private int id;
+  private String name;
+public void setName(String n)
+{
+    this.name=n;
+}
+public String getName()
+{
+    return name;
+}
+public void setId(int i)
+{
+    this.id=i;
+}
+public int getId()
+{
+    return id;
+}
+}
+
+public class GetterSetter {
+    public static void main(String[] args) {
+        Employee emp=new Employee();
+       emp.setName("vinay");
+       emp.setId(12);
+        System.out.println(emp.getName());
+        System.out.println(emp.getId());
+    }
 }
